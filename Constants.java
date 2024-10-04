2bf4b97e8eeac034d8b713fcf83ce159c6c85314
Habibur Rahman <hr13338@gmail.com>
2024-10-04 22:58:56 +0600

    STEP #5.Replaced string literals with constants, storing those constants in a new class called Constants.java

---
+++ Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    
+    protected final String LOADING_MESSAGE = "Loading Data For Your Command . Please wait with paitance .";
+    protected final String LOADED_MESSAGE = "your required data is loded sucessfully";
+    protected final String DATE_FORMATE = "dd/mm/yyyy-hh:mm:ss a";
+    protected final String FOUND_MESSAGE = "We found it!";
+}
